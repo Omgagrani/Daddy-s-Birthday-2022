21f23280f6290c5bbc8a1e5445503f80652bfab2
Omgagrani <103943996+Omgagrani@users.noreply.github.com>
2022-08-09 23:17:16 -0400

    Daddy's Birthday 2022

---
+++ om's gift/The Game.py
@@ -0,0 +1,17 @@
+def start():
+    print(" ")
+    print("Welcome to The Game")
+    input()
+    print("Here, you will be asked random questions")
+    input()
+    print("Each question you get correct, you will get a point")
+    input()
+    print("The person with the most points wins")
+    input()
+    print("The person with the least point has to get a punishment(The winner will decide the punishment)")
+    input()
+    
+def getPlayers():
+    print("How many player are going to play the game?")
+
+start()
